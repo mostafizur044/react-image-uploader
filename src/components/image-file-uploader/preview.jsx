ff4fbb3848746f13d5a63915e3b2bc62af5e3f80
mostafizur044 <mostafiz.mri@gmail.com>
2019-02-12 23:03:31 +0600

    File name added

--- src/components/image-file-uploader/preview.jsx
+++ src/components/image-file-uploader/preview.jsx
@@ -3,12 +3,11 @@ import React from "react";
 
 const Preview = ({ imagePreview, width, height, removeFile }) => {
   const divStyle = {
-    width: width ? width : 100,
-    height: height ? height : 100
+    width: width ? width : 120,
   };
   const imgStyle = {
-    width: width ? width - 10 : 100,
-    height: height ? height - 10 : 100
+    width: width ? width - 10 : 120,
+    height: height ? height - 10 : 120
   };
 
   return (
@@ -16,9 +15,11 @@ const Preview = ({ imagePreview, width, height, removeFile }) => {
       {imagePreview.map((img, i) => {
         return (
           <div key={i} className="previewImage" style={divStyle}>
-            <img src={getCorrectImage(img)} alt="no image" style={imgStyle} />
+            <div>
+              <img src={getCorrectImage(img)} alt="no image" style={imgStyle} />
+              {getName(img)}
+            </div>
             <div className="overlay">
-              {previewFile(img)}
               <span
                 className="btn btn-sm btn-custom-reset"
                 onClick={() => removeFile(i)}
@@ -34,15 +35,26 @@ const Preview = ({ imagePreview, width, height, removeFile }) => {
 };
 
 function getCorrectImage(img) {
+  console.log(img)
   if (typeof img === "string") return img;
-  return "https://cdn0.iconfinder.com/data/icons/iconico-3/1024/63.png";
+  return "https://www.balcousa.com/wp-content/uploads/2017/11/unnamed.png";
 }
 
-function previewFile(img) {
-  if(typeof img === "object") {
-    const file = URL.createObjectURL(img);
-  return <a className="btn btn-sm btn-custom-reset" href={file} target="_blank">Preview</a>
+function getName(img) {
+  if (typeof img === "object") {
+    return <small>Name: {img.name}, Size: {getSize(img.size)}</small>
   }
+  return '';
+}
+
+function getSize(size) {
+  let totalSize;
+  if(size < 1000000){
+    totalSize = Math.floor(size/1000) + 'KB';
+ }else{
+    totalSize = Math.floor(size/1000000) + 'MB';  
+ }
+ return totalSize;
 }
 
 export default Preview;
